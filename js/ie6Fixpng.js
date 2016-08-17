7effe86a58d9473ee471e494590f995089eebd25
xiaoyusis <jiayouxxt38@163.com>
2016-08-17 20:42:59 +0800

    Add first web
    learning how to design the web by mooc

---
+++ js/ie6Fixpng.js
@@ -0,0 +1,25 @@
+/**
+ * Created by hp on 2016/8/17.
+ */
+DD_belatedPNG.fix('div');
+DD_belatedPNG.fix('button');
+DD_belatedPNG.fix('a');
+DD_belatedPNG.fix('h1');
+DD_belatedPNG.fix('h2');
+DD_belatedPNG.fix('h3');
+DD_belatedPNG.fix('h4');
+DD_belatedPNG.fix('h5');
+DD_belatedPNG.fix('h6');
+DD_belatedPNG.fix('em');
+DD_belatedPNG.fix('strong');
+DD_belatedPNG.fix('dl dt dd');
+DD_belatedPNG.fix('form');
+DD_belatedPNG.fix('p img');
+DD_belatedPNG.fix('a img');
+DD_belatedPNG.fix('img');
+DD_belatedPNG.fix('input');
+DD_belatedPNG.fix('li');
+DD_belatedPNG.fix('td');
+DD_belatedPNG.fix('span');
+DD_belatedPNG.fix('table tr td');
+//注意：不要偷懒用*，性能很低
